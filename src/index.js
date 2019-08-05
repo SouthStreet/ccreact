e30c2694cd4a23c59d866d900434aa00ab4128dc
SouthStreet <znj764359824@gmail.com>
2019-08-05 14:12:56 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Todolist from './Todolist';//App.js 组件开头大写！
+//jsx
+ReactDOM.render(<Todolist/>, document.getElementById('root'));
+//app 组件挂载到dom下
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
